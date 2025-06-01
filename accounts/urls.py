7c22ab3679969ea18e3cae6966a0ab3d53f741c7
Hesam Norouzi <m.hesam.norouzi@gmail.com>
2025-06-01 10:21:35 +0330

    Implement password reset functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -19,5 +19,9 @@ urlpatterns = [
     path('password_change/done/', 
          auth_views.PasswordChangeDoneView.as_view(template_name='accounts/password_change_done.html'), 
          name='password_change_done'),
-     path('delete/', delete_account_view, name='delete_account'),
+    path('delete/', delete_account_view, name='delete_account'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(extra_email_context = {"protocol": "https", "domain": "django.hesamnorouzi.ir"}), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
